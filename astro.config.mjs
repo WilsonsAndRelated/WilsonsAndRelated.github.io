3d052e69d4a83ba7addb4d83d35b18b851246a33
Andrew <andrew@noreply.localhost>
2025-08-06 11:42:33 -0600

    Removing the last remnants of the sample template

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,13 +11,6 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/WilsonsAndRelated/WilsonsAndRelated.github.io' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
 				},
