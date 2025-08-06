88ffbede69e0e3e4016707de57765d209872d2f3
Andrew <andrew@noreply.localhost>
2025-08-06 02:06:44 -0600

    Next style iteration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Wilsons and Related',
+  tagline: 'Fantasy Football League',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -73,7 +73,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Wilsons and Related',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -83,7 +83,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Rulebook',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/rulebook">
-            Docusaurus Tutorial - 5min ⏱️
+            View the Rulebook
           </Link>
         </div>
       </div>
